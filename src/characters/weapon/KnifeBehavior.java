602d146887183183f28c96694a16a454a2f151ab
VitStar999 <vitalis.khomenko999@gmail.com>
2024-08-04 19:30:00 +0300

    Добавив реалізацію зброї та створий нові об'єкти

--- src/characters/weapon/KnifeBehavior.java
+++ src/characters/weapon/KnifeBehavior.java
@@ -1,10 +1,7 @@
 package characters.weapon;
 
 public class KnifeBehavior implements WeaponBehavior{
-    WeaponBehavior  sword = new SwordBehavior();
-    WeaponBehavior  bow_and_arrow = new BowAndArrowBehavior();
-    WeaponBehavior  knife = new KnifeBehavior();
-    WeaponBehavior  axe = new AxeBehavior();
+
 
     @Override
     public void useWeapon() {
